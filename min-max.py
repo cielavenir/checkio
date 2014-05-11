07bb40c65edfa245d93bd7ee261c5d2f13ce2c80
cielavenir <cielartisan@gmail.com>
2014-05-11 23:08:28 +0900

    word-pattern

--- min-max.py
+++ min-max.py
@@ -1,30 +1,25 @@
 #import collections
 def iterable(obj):
 	#return isinstance(args,collections.Iterable)
+	#return hasattr(obj,'__iter__') # only Py3
 	try:
 		iter(obj)
 		return True
 	except TypeError:
 		return False
 
-def min2(cur,ite,key):
+def minmax2(cur,ite,compare,key):
 	try:
 		nxt=next(ite)
-		return min2(nxt if key(nxt)<key(cur) else cur,ite,key=key)
+		return minmax2(nxt if compare(key(nxt),key(cur)) else cur,ite,compare,key)
 	except StopIteration:
 		return cur
 def min(*args,**kwargs):
 	ite=iter(args[0]) if len(args)==1 and iterable(args[0]) else iter(args)
-	return min2(next(ite),ite,key=kwargs.get('key',lambda x:x))
-def max2(cur,ite,key):
-	try:
-		nxt=next(ite)
-		return max2(nxt if key(nxt)>key(cur) else cur,ite,key=key)
-	except StopIteration:
-		return cur
+	return minmax2(next(ite),ite,lambda x,y: x<y,kwargs.get('key',lambda x:x))
 def max(*args,**kwargs):
 	ite=iter(args[0]) if len(args)==1 and iterable(args[0]) else iter(args)
-	return max2(next(ite),ite,key=kwargs.get('key',lambda x:x))
+	return minmax2(next(ite),ite,lambda x,y: x>y,kwargs.get('key',lambda x:x))
 
 if __name__ == '__main__':
     assert max(3, 2) == 3, "Simple case max"
