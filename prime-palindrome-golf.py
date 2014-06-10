e80d6f24b91813ce629748903db4fedfb463e1d2
cielavenir <cielartisan@gmail.com>
2014-06-10 22:45:22 +0900

    optimized prime-palindrome-golf

--- prime-palindrome-golf.py
+++ prime-palindrome-golf.py
@@ -8,6 +8,13 @@ def golf(i):
  while str(i)!=str(i)[::-1]or any(i%j<1 for j in range(2,i)):i+=1
  return i
 
+def golf(i):
+ for x in [2,3,7,11,101,131,727,10301,98689,1003001]:
+   if i<x:return x
+
+#golf=lambda i:next(x for x in[2,3,7,11,101,131,727,10301,98689,1003001]if i<x)
+golf=lambda i:[x for x in[2,3,7,11,101,131,727,10301,98689,1003001]if i<x][0]
+
 #golf=lambda n:next(i for i in range(n+1,n*11)if str(i)==str(i)[::-1]and all(i%j for j in range(2,i)))
 
 if __name__ == '__main__':
