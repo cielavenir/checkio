fc1fdb56c7d4a54cbe983855de5b2b0c32d03eea
cielavenir <cielartisan@gmail.com>
2014-06-10 14:31:13 +0900

    prime-palindrome-golf

---
+++ prime-palindrome-golf.py
@@ -0,0 +1,19 @@
+#def golf(i):
+# while 1:
+#  i+=1
+#  if str(i)==str(i)[::-1]and all(i%j for j in range(2,i)):return i
+
+def golf(i):
+ i+=1
+ while str(i)!=str(i)[::-1]or any(i%j<1 for j in range(2,i)):i+=1
+ return i
+
+#golf=lambda n:next(i for i in range(n+1,n*11)if str(i)==str(i)[::-1]and all(i%j for j in range(2,i)))
+
+if __name__ == '__main__':
+	assert golf(2)==3
+	assert golf(13)==101
+	assert golf(101)==131
+	assert golf(9000)==10301
+	assert golf(999999)==1003001
+	assert golf(1)==2
--- pycontw2014/simple2048.py
+++ pycontw2014/simple2048.py
@@ -9,21 +9,23 @@ def twoeleven_iterate(iterable,z):
 		i+=1
 	return list(z(squeezed))+[0]*(4-len(squeezed))
 
-def checkio(a, b):
+def move2048(a, b):
 	if b=='up': x=list(map(list,zip(*[twoeleven_iterate([a[j][i] for j in range(4) if a[j][i]!=0],list) for i in range(4)])))
 	if b=='down': x=list(map(list,zip(*[list(reversed(twoeleven_iterate([a[j][i] for j in range(4) if a[j][i]!=0],reversed))) for i in range(4)])))
 	if b=='right': x=[list(reversed(twoeleven_iterate([a[i][j] for j in range(4) if a[i][j]!=0],reversed))) for i in range(4)]
 	if b=='left': x=[twoeleven_iterate([a[i][j] for j in range(4) if a[i][j]!=0],list) for i in range(4)]
-	if a==x: return x
+	for i in range(4):
+		if any(x[i][j]==2048 for j in range(4)):
+			return [['U','W','I','N']]*4
 	for i in reversed(range(4)):
 		for j in reversed(range(4)):
 			if x[i][j]==0:
 				x[i][j]=2
 				return x
-	return x
+	return [['G','A','M','E'],['O','V','E','R']]*2
 
 if __name__ == '__main__':
-	assert checkio([[0,0,0,0],[0,0,0,0],[0,0,2,0],[0,2,0,0]], 'up') == [[0,2,2,0],[0,0,0,0],[0,0,0,0],[0,0,0,2]] 
-	assert checkio([[0,0,0,0],[0,0,0,0],[0,0,0,0],[2,2,2,2]], 'left') == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[4,4,0,2]]
-	assert checkio([[0,0,0,0],[0,0,0,0],[0,0,0,0],[4,4,0,2]], 'right') == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,2,8,2]]
+	assert move2048([[0,0,0,0],[0,0,0,0],[0,0,2,0],[0,2,0,0]], 'up') == [[0,2,2,0],[0,0,0,0],[0,0,0,0],[0,0,0,2]] 
+	assert move2048([[0,0,0,0],[0,0,0,0],[0,0,0,0],[2,2,2,2]], 'left') == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[4,4,0,2]]
+	assert move2048([[0,0,0,0],[0,0,0,0],[0,0,0,0],[4,4,0,2]], 'right') == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,2,8,2]]
 	print('Done')
