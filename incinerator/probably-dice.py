3fbf98589c41211de0a83facc64075330ea172ca
cielavenir <cielartisan@gmail.com>
2014-06-11 19:41:45 +0900

    published probably-dice

--- incinerator/probably-dice.py
+++ incinerator/probably-dice.py
@@ -1,5 +1,5 @@
 #import itertools
-def checkio(n, s, t):
+def probability(n, s, t):
 	a=[0]*(s*(n+1)+1)
 	for i in range(1,s+1):
 		a[i+s]=1.0/s**n
@@ -16,10 +16,10 @@ if __name__ == '__main__':
 	def almost_equal(checked, correct, significant_digits=4):
 		precision = 0.1 ** significant_digits
 		return correct - precision < checked < correct + precision
-	assert(almost_equal(checkio(2, 6, 3), 0.0556))
-	assert(almost_equal(checkio(2, 6, 4), 0.0833))
-	assert(almost_equal(checkio(2, 6, 7), 0.1667))
-	assert(almost_equal(checkio(2, 3, 5), 0.2222))
-	assert(almost_equal(checkio(2, 3, 7), 0.0000))
-	assert(almost_equal(checkio(3, 6, 7), 0.0694))
-	assert(almost_equal(checkio(10, 10, 50), 0.0375))
+	assert(almost_equal(probability(2, 6, 3), 0.0556))
+	assert(almost_equal(probability(2, 6, 4), 0.0833))
+	assert(almost_equal(probability(2, 6, 7), 0.1667))
+	assert(almost_equal(probability(2, 3, 5), 0.2222))
+	assert(almost_equal(probability(2, 3, 7), 0.0000))
+	assert(almost_equal(probability(3, 6, 7), 0.0694))
+	assert(almost_equal(probability(10, 10, 50), 0.0375))
