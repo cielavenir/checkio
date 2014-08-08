77d0cc7a61cd61f6cb2d66573cf23d1ab587b3c2
cielavenir <cielartisan@gmail.com>
2014-08-08 16:43:01 +0900

    one-line-drawing self-test

--- one-line-drawing.py
+++ one-line-drawing.py
@@ -24,7 +24,6 @@ def draw(segments):
 		if len(points[p])%2==1: odd.append(p)
 	if 1==len(odd) or 2<len(odd): return []
 	cur,end=(odd[0],odd[1]) if odd else (q,q)
-	print(odd)
 	num=defaultdict(int)
 	num[cur]+=1
 	num[end]-=1
@@ -34,8 +33,40 @@ def draw(segments):
 		if num[nxt]<=len(points[nxt]):
 			r=dfs([list(sorted([cur,nxt]))],nxt,points,num)
 		num[nxt]-=2
-		if r!=None: print([cur]+r)
+		if r!=None: return [cur]+r
 
-draw({(1,2,1,5),(1,2,7,2),(1,5,4,7),(4,7,7,5)})
-draw({(1,2,1,5),(1,2,7,2),(1,5,4,7),(4,7,7,5),(7,5,7,2),(1,5,7,2),(7,5,1,2)})
-draw({(1,2,1,5),(1,2,7,2),(1,5,4,7),(4,7,7,5),(7,5,7,2),(1,5,7,2),(7,5,1,2),(1,5,7,5)})
+if __name__ == '__main__':
+	def checker(func, in_data, is_possible=True):
+		user_result = func(in_data)
+		if not is_possible:
+			if user_result:
+				print("How did you draw this?")
+				return False
+			else:
+				return True
+		if len(user_result) < 2:
+			print("More points please.")
+			return False
+		data = list(in_data)
+		for i in range(len(user_result) - 1):
+			f, s = user_result[i], user_result[i + 1]
+			if (f + s) in data:
+				data.remove(f + s)
+			elif (s + f) in data:
+				data.remove(s + f)
+			else:
+				print("The wrong segment {}.".format(f + s))
+				return False
+		if data:
+			print("You forgot about {}.".format(data[0]))
+			return False
+		return True
+	assert checker(draw,
+				   {(1, 2, 1, 5), (1, 2, 7, 2), (1, 5, 4, 7), (4, 7, 7, 5)}), "Example 1"
+	assert checker(draw,
+				   {(1, 2, 1, 5), (1, 2, 7, 2), (1, 5, 4, 7),
+					(4, 7, 7, 5), (7, 5, 7, 2), (1, 5, 7, 2), (7, 5, 1, 2)},
+				   False), "Example 2"
+	assert checker(draw,
+				   {(1, 2, 1, 5), (1, 2, 7, 2), (1, 5, 4, 7), (4, 7, 7, 5),
+					(7, 5, 7, 2), (1, 5, 7, 2), (7, 5, 1, 2), (1, 5, 7, 5)}), "Example 3"
