55ba2c39de452f727c5c8fa0b0aface704731571
cielavenir <cielartisan@gmail.com>
2014-08-21 00:16:20 +0900

    update bullet-and-wall

---
+++ bag-of-santa-claus.py
@@ -0,0 +1,51 @@
+def choose_good_gift(total_gifts, bag, accept_gift):
+	n=total_gifts//2
+	gifts=[]
+	for i in range(n):
+		gifts.append(next(bag()))
+	value=int(1.1*sum(gifts)/n)
+	for e in bag():
+		if e > value:
+			accept_gift()
+			return
+
+
+if __name__ == '__main__':
+	from random import random, randint, uniform
+	def priority_post_factum(gifts):
+		def do_accept():
+			nonlocal gift_value
+			if gift_value is None:
+				if idx < len(gifts):
+					gift_value = gifts[idx]
+				else:
+					print("Is that a joke - to say 'accept' when"
+						  " gift wasn't taken from the bag?")
+			else:
+				print('Sorry, you made your choice already.')
+		def gift_generator():
+			nonlocal idx
+			while idx:
+				idx -= 1
+				yield gifts[idx]
+		idx, gift_value = len(gifts), None
+		choose_good_gift(idx, gift_generator, do_accept)
+		if gift_value is None:
+			print('Unfortunately, you did not choose anything.')
+			return len(gifts)
+		else:
+			return sum(gift_value < x for x in gifts)
+	def check_solution(bag_count):
+		standings = gift_count = best_gifts = 0
+		for i in range(bag_count):
+			gifts_in_bag = randint(10, 1000)
+			gift_count += gifts_in_bag
+			scale = (random() + random()) ** randint(0, 1024)
+			priority = priority_post_factum([uniform(0., scale) for _ in range(gifts_in_bag)])
+			standings += priority
+			best_gifts += not priority
+		print('You do won {:n} best gifts from {:n} bags with {:,} gifts!\n'
+			  'It seems like for bags of {:n} gifts -\n'
+			  'you would choose the second best gift, silver ;)'
+			  .format(best_gifts, bag_count, gift_count, round(gift_count / standings) + 1))
+	check_solution(1000)
--- old-library/bullet-and-wall.py
+++ old-library/bullet-and-wall.py
@@ -1,18 +1,27 @@
 cross=lambda a,b: (a.conjugate()*b).imag
 dot=lambda a,b: (a.conjugate()*b).real
 def crosspoint(s0,s1,l0,l1):
-	A = cross(l1-l0,s1-s0)
+	A = cross(l1-l0,s1-s0) # 
 	B = cross(l1-l0,l1-s0)
 	if abs(A)<1e-7:
 		if abs(B)<1e-7:
 			C=s0
 		else:
-			return False
+			return -1
 	else:
 		C = s0 + B/A*(s1-s0)
-	return dot(s0-C,s1-C)<1e-7 and dot(C-l0,l1-l0)>-1e-7
+	# now, C is the crosspoint of s0s1 and l0l1
+	if dot(s0-C,s1-C)<1e-7 and dot(C-l0,l1-l0)>-1e-7: #C is on s0s1 and C is beyond l0
+		ratio=2*abs(C-s0)/abs(s1-s0)
+		if ratio>1: ratio=2-ratio
+		#print(ratio)
+		return round(100*ratio)
+	else:
+		return -1
 
-checkio=lambda data: crosspoint(*[complex(*e) for e in data])
+shot=lambda *data: crosspoint(*[complex(*e) for e in data])
+#old-library bullet-and-wall compatibility
+checkio=lambda data: crosspoint(*[complex(*e) for e in data])>=0
 
 if __name__ == '__main__':
 	assert checkio([[0, 0], [0, 2], [5, 1], [3, 1]]) == True, "1st example"
@@ -21,3 +30,8 @@ if __name__ == '__main__':
 	assert checkio([[6, 0], [5, 5], [4, 0], [5, 6]]) == False, "4th example"
 	assert checkio([[0, 0], [1, 1], [3, 3], [2, 2]]) == True, "5th example"
 	assert checkio([[0, 0], [1, 1], [3, 2], [2, 1]]) == False, "6th example"
+	assert shot((2, 2), (5, 7), (11, 2), (8, 3)) == 100, "1st case"
+	assert shot((2, 2), (5, 7), (11, 2), (7, 2)) == 0, "2nd case"
+	assert shot((2, 2), (5, 7), (11, 2), (8, 4)) == 33, "3th case"
+	assert shot((2, 2), (5, 7), (11, 2), (9, 5)) == -1, "4th case"
+	assert shot((2, 2), (5, 7), (11, 2), (10.5, 3)) == -1, "4th case again"
