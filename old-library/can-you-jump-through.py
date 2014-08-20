284fa2415e1e5b6809c568858168c1d2d942fe81
cielavenir <cielartisan@gmail.com>
2014-08-20 11:47:17 +0900

    update can-you-jump-through

--- old-library/can-you-jump-through.py
+++ old-library/can-you-jump-through.py
@@ -1,15 +1,19 @@
-def dfs(data,x,y,xg,yg):
-	if y<0 or len(data)<=y or x<0 or len(data[0])<=x or data[y][x]!=0: return False
+def dfs(data,x,y,xg,yg,c):
+	if y<0 or len(data)<=y or x<0 or len(data[0])<=x or data[y][x]!=c: return False
 	if x==xg and y==yg: return True
-	data[y][x]=1
-	if dfs(data,x,y-1,xg,yg): return True
-	if dfs(data,x-1,y,xg,yg): return True
-	if dfs(data,x+1,y,xg,yg): return True
-	if dfs(data,x,y+1,xg,yg): return True
-	data[y][x]=0
+	data[y][x]=-1
+	if dfs(data,x,y-1,xg,yg,c): return True
+	if dfs(data,x-1,y,xg,yg,c): return True
+	if dfs(data,x+1,y,xg,yg,c): return True
+	if dfs(data,x,y+1,xg,yg,c): return True
+	data[y][x]=c
 	return False
 
-checkio=lambda data: dfs(data[0],data[1][1]-1,data[1][0]-1,data[2][1]-1,data[2][0]-1)
+dfs_=lambda data,x,y,xg,yg:dfs(data,x,y,xg,yg,data[y][x])
+
+can_pass=lambda data,first,second: dfs_(list(map(list,data)),first[1],first[0],second[1],second[0])
+#old library can-you-jump-through compatibility
+checkio=lambda data: dfs_(data[0],data[1][1]-1,data[1][0]-1,data[2][1]-1,data[2][0]-1,)
 
 if __name__ == '__main__':
 	assert checkio(([
@@ -26,3 +30,19 @@ if __name__ == '__main__':
 		[8, 0, 0, 4, 0],
 		[0, 9, 0, 1, 0]],
 		[1,1], [1,5]]) == False, 'Second example'
+	assert can_pass(((0, 0, 0, 0, 0, 0),
+					 (0, 2, 2, 2, 3, 2),
+					 (0, 2, 0, 0, 0, 2),
+					 (0, 2, 0, 2, 0, 2),
+					 (0, 2, 2, 2, 0, 2),
+					 (0, 0, 0, 0, 0, 2),
+					 (2, 2, 2, 2, 2, 2),),
+					(3, 2), (0, 5)) == True, 'First example'
+	assert can_pass(((0, 0, 0, 0, 0, 0),
+					 (0, 2, 2, 2, 3, 2),
+					 (0, 2, 0, 0, 0, 2),
+					 (0, 2, 0, 2, 0, 2),
+					 (0, 2, 2, 2, 0, 2),
+					 (0, 0, 0, 0, 0, 2),
+					 (2, 2, 2, 2, 2, 2),),
+					(3, 3), (6, 0)) == False, 'First example'
--- old-library/musical-module_update.py
+++ old-library/musical-module_update.py
@@ -2,6 +2,8 @@ from functools import reduce
 from fractions import gcd
 #gcd=lambda a,b: a if b==0 else gcd(b,a%b)
 greatest_common_divisor=lambda *args:reduce(gcd,args)
+#old library musical-module compatibility
+checkio=lambda args:greatest_common_divisor(*args)
 	
 if __name__ == '__main__':
 	assert greatest_common_divisor(6, 4) == 2, "Simple"
