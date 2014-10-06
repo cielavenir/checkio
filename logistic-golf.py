a70efc0c2851a5a38270b41b281997ff6c8551c1
cielavenir <cielartisan@gmail.com>
2014-10-07 01:16:36 +0900

    optimized logistic-golf

--- logistic-golf.py
+++ logistic-golf.py
@@ -1,12 +1,11 @@
 def golf(z):
-	y=[list(e)for e in z]
-	l=len(y)
-	for k in range(l):
-		for i in range(l):
-			for j in range(l):
-				if i!=j and y[i][k] and y[j][k] and (y[i][j]==0 or y[i][j]>y[i][k]+y[k][j]):
-					y[i][j]=y[i][k]+y[k][j]
-	return y[0][l-1]
+	y=list(map(list,z))
+	l=range(len(y))
+	for k in l:
+		for i in l:
+			for j in l:
+				if y[i][k]*y[k][j]:y[i][j]=min(y[i][j]or 1e9,y[i][k]+y[k][j])
+	return y[0][len(y)-1]
 
 if __name__=='__main__':
 	assert golf(((0, 80, 58, 0), (80, 0, 71, 80), (58, 71, 0, 58), (0, 80, 58, 0))) == 116
