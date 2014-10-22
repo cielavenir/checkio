a9017ddac4da5fe5be7d83456b91fce8ae975fcb
cielavenir <cielartisan@gmail.com>
2014-10-22 14:59:10 +0900

    self test

--- clock-angle.py
+++ clock-angle.py
@@ -5,5 +5,10 @@ def clock_angle(s):
 	return min(360-abs(h-m),abs(h-m))
 
 if __name__=='__main__':
-	print(clock_angle('02:30'))
-	print(clock_angle('13:42'))
+	assert clock_angle("02:30") == 105, "02:30"
+	assert clock_angle("13:42") == 159, "13:42"
+	assert clock_angle("01:42") == 159, "01:42"
+	assert clock_angle("01:43") == 153.5, "01:43"
+	assert clock_angle("00:00") == 0, "Zero"
+	assert clock_angle("12:01") == 5.5, "Little later"
+	assert clock_angle("18:00") == 180, "Opposite"
