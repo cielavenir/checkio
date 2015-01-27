859de317c93e9d50774c035c01454b958770cb46
cielavenir <cielartisan@gmail.com>
2015-01-27 23:29:51 +0900

    update einstein-problem

--- einstein-problem.py
+++ einstein-problem.py
@@ -1,39 +1,37 @@
-colors        = ['blue', 'green', 'red', 'white', 'yellow']
-pets          = ['cat', 'bird', 'dog', 'fish', 'horse']
-potables      = ['beer', 'coffee', 'milk', 'tea', 'water'] 
-cigarettes    = ['Rothmans', 'Dunhill', 'Pall Mall', 'Winfield' , 'Marlboro']
-person        = ['Brit', 'Dane', 'German', 'Norwegian', 'Swede'] 
-num           = ['1','2','3','4','5']
-point         = [colors,pets,potables,cigarettes,person,num]  
+color       = ['blue', 'green', 'red', 'white', 'yellow']
+pet         = ['cat', 'bird', 'dog', 'fish', 'horse']
+beverage    = ['beer', 'coffee', 'milk', 'tea', 'water'] 
+cigarettes  = ['Rothmans', 'Dunhill', 'Pall Mall', 'Winfield' , 'Marlboro']
+nationality = ['Brit', 'Dane', 'German', 'Norwegian', 'Swede'] 
+number      = ['1','2','3','4','5']
 
 import itertools
-lst=list(itertools.product(*point))
-
-def checkio(data,question):
+lst=list(itertools.product(color,pet,beverage,cigarettes,nationality,number))
+def answer(data,question):
 	processed = [l for l in lst if all(sum(e in l for e in d.split('-'))!=1 for d in data)]
-	q = question[0].split('-')
+	q = question.split('-')
 	row = next(e for e in processed if q[0] in e)
 	return next(e for e in globals()[q[1]] if e in row)
 
 if __name__ == '__main__':
-	assert checkio(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+	assert answer(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
 					'German-coffee','beer-white','cat-water',
 					'horse-2','milk-3','4-Rothmans',
 					'dog-Swede','Norwegian-1','horse-Marlboro',
 					'bird-Brit','4-green','Winfield-beer',
 					'Dane-blue','5-dog','blue-horse',
-					'yellow-cat','Winfield-Swede','tea-Marlboro'],['fish-colors'])=='green' #1st test
-	assert checkio(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+					'yellow-cat','Winfield-Swede','tea-Marlboro'],'fish-color')=='green' #1st test
+	assert answer(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
 					'German-coffee','beer-white','cat-water',
 					'horse-2','milk-3','4-Rothmans',
 					'dog-Swede','Norwegian-1','horse-Marlboro',
 					'bird-Brit','4-green','Winfield-beer',
 					'Dane-blue','5-dog','blue-horse',
-					'yellow-cat','Winfield-Swede','tea-Marlboro'],['tea-num'])=='2' #2st test
-	assert checkio(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+					'yellow-cat','Winfield-Swede','tea-Marlboro'],'tea-number')=='2' #2st test
+	assert answer(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
 					'German-coffee','beer-white','cat-water',
 					'horse-2','milk-3','4-Rothmans',
 					'dog-Swede','Norwegian-1','horse-Marlboro',
 					'bird-Brit','4-green','Winfield-beer',
 					'Dane-blue','5-dog','blue-horse',
-					'yellow-cat','Winfield-Swede','tea-Marlboro'],['Norwegian-potables'])=='water' #3st test
+					'yellow-cat','Winfield-Swede','tea-Marlboro'],'Norwegian-beverage')=='water' #3st test
