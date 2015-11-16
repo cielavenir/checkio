629dd4a3710e86f3dfd6aaa39face15faca05f77
cielavenir <cielartisan@gmail.com>
2015-11-17 03:50:36 +0900

    univocalic_davasaan is now stable

--- codeship/univocalic_davasaan.py
+++ codeship/univocalic_davasaan.py
@@ -1,5 +1,6 @@
 #davasaan=lambda x:x*0xcccccccd>>35
-davasaan=lambda x:x+(x<<2)+(x<<3)+(x<<6)+(x<<7)+(x<<10)+(x<<11)+(x<<14)+(x<<15)+(x<<18)+(x<<19)+(x<<22)+(x<<23)+(x<<26)+(x<<27)+(x<<30)+(x<<31)>>35
+#davasaan=lambda x:x+(x<<2)+(x<<3)+(x<<6)+(x<<7)+(x<<10)+(x<<11)+(x<<14)+(x<<15)+(x<<18)+(x<<19)+(x<<22)+(x<<23)+(x<<26)+(x<<27)+(x<<30)+(x<<31)>>35
+f=lambda x,n:x and f(x>>1,n+n)+[0,n][x%2];davasaan=lambda x:f(x,3435973837)>>35
 
 if __name__ == '__main__':
 	assert davasaan(0) == 0, "First"
