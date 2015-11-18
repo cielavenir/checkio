7363baf122f1a3118f38ec71d2e3fa18686e4246
cielavenir <cielartisan@gmail.com>
2015-11-19 04:03:20 +0900

    optimized house-password

--- home/house-password.py
+++ home/house-password.py
@@ -1,6 +1,6 @@
 #checkio=lambda data: len(data)>=10 and any(e.isdigit() for e in data) and any(e.islower() for e in data) and any(e.isupper() for e in data)
 
-checkio=lambda d:len(d)>9 and all(any(getattr(e,z)()for e in d)for z in['isdigit','islower','isupper'])
+checkio=lambda d:len(d)>9 and all(any(getattr(e,'is'+z)()for e in d)for z in['digit','lower','upper'])
 
 if __name__ == '__main__':
 	assert checkio('A1213pokl') == False, "1st example"
