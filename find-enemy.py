2aee5f2d25c53edfa3f1aefe2b4431fcb693f3d6
cielavenir <cielartisan@gmail.com>
2016-10-26 21:36:40 +0900

    find-enemy

---
+++ find-enemy.py
@@ -0,0 +1,39 @@
+import math
+def convert(s):
+	ax=ord(s[0])-65
+	a=[float(ax)*math.sqrt(3)/2,-float(s[1:])+1]
+	if ax%2: a[1]-=0.5
+	return a
+
+T=['N','NW','SW','S','SE','NE']
+def find_enemy(a_,d,b_):
+	eps=1e-9
+	a=convert(a_)
+	b=convert(b_)
+	b[0]-=a[0]
+	b[1]-=a[1]
+	arg=math.atan2(b[1],b[0])-math.pi*T.index(d)/3
+	if arg<-math.pi: arg+=math.pi
+	if arg<-math.pi: arg+=math.pi
+	if (math.pi*1/6)+eps<arg<-eps+math.pi*5/6:
+		d='F'
+	elif math.pi*-5/6+eps<arg<-eps+math.pi*-1/6:
+		d='B'
+	elif abs(arg)<=math.pi*1/2:
+		# math.pi*-1/6<=arg<=math.pi*1/6:
+		d='R'
+	else:
+		d='L'
+	return [d,int(max(abs(b[1]),abs(b[0])*2/math.sqrt(3)+eps))]
+
+if __name__ == '__main__':
+	assert find_enemy('G5', 'N', 'G4') == ['F', 1], "N-1"
+	assert find_enemy('G5', 'N', 'I4') == ['R', 2], "NE-2"
+	assert find_enemy('G5', 'N', 'J6') == ['R', 3], "SE-3"
+	assert find_enemy('G5', 'N', 'G9') == ['B', 4], "S-4"
+	assert find_enemy('G5', 'N', 'B7') == ['L', 5], "SW-5"
+	assert find_enemy('G5', 'N', 'A2') == ['L', 6], "NW-6"
+	assert find_enemy('G3', 'NE', 'C5') == ['B', 4], "[watch your six!]"
+	assert find_enemy('H3', 'SW', 'E2') == ['R', 3], "right"
+	assert find_enemy('A4', 'S', 'M4') == ['L', 12], "true left"
+	print("You are good to go!")
