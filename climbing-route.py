4a471945d3bd02d4f12a859e78a5e7bdec658064
cielavenir <cielartisan@gmail.com>
2017-02-14 12:58:38 +0900

    update climbing-route

--- climbing-route.py
+++ climbing-route.py
@@ -1,5 +1,6 @@
 import heapq
 INF=1<<30
+D=((-1,0),(0,-1),(1,0),(0,1))
 
 def shortestPath(g,s,dist,prev):
 	n = len(g)
@@ -30,12 +31,23 @@ def shortestHamiltonCycle(n):
 						best[S|(1<<j)][j] = best[S][i] + M[i][j]
 	return -1 if best[N-1][0]==INF else best[N-1][0]
 
+def inside(data,y,x):
+	return 0<=y<len(data) and 0<=x<len(data[0])
+
+def mountaintop(data,y,x):
+	if not inside(data,y,x) or data[y][x]==0: return (0,y,x)
+	r=(data[y][x],y,x)
+	data[y][x]=0
+	for dx,dy in D:
+		q=mountaintop(data,y+dy,x+dx)
+		if r[0]<q[0]: r=q
+	return r
+
 def climbing_route(data):
 	global M
 	data=[list(map(int,e)) for e in data]
 	H=len(data)
 	W=len(data[0])
-	max_area=[0,H*W-1]
 	g=[[] for _ in range(H*W)]
 	for i in range(H):
 		for j in range(W):
@@ -48,12 +60,18 @@ def climbing_route(data):
 				y=i*W+j+1
 				f=data[i][j]-data[i][j+1]
 				if abs(f)<=1: g[x].append((1,x,y));g[y].append((1,y,x))
-			#too long line; I miss Ruby
-			if all(
-				j+dx<0 or W<=j+dx or i+dy<0 or H<=i+dy or data[i][j]>data[i+dy][j+dx]
-				for dx,dy in ((-1,0),(0,-1),(1,0),(0,1))
-			):
+	#in this iteration "data" will be destroyed
+	max_area=[0,H*W-1]
+	for i in range(H):
+		for j in range(W):
+			x=i*W+j
+			'''
+			if all(not inside(data,i+dy,j+dx) or data[i][j]>data[i+dy][j+dx] for dx,dy in D):
 				max_area.append(x)
+			'''
+			if data[i][j]>0:
+				top=mountaintop(data,i,j)
+				max_area.append(top[1]*W+top[2])
 	N=len(max_area)
 	M=[[0]*N for _ in range(N)]
 	for i in range(N):
@@ -85,10 +103,10 @@ if __name__ == '__main__':
 		'012100002220',
 		'011100000000']) == 26, 'two top'
 	assert climbing_route([
-		'00000000210000',
+		'00000000120000',
 		'00100001432100',
 		'01211112211000',
-		'00100000000000']) == 22, 'one top'
+		'00100000000000']) == 18, 'one top'
 	assert climbing_route([
 		'00000000000000000000000000',
 		'00000000000111111100000000',
